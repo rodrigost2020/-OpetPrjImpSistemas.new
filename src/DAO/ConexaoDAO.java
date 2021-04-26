62efad6cb8292bb058b3b04b55b764a61e51bb51
rodrigost2020 <70176785+rodrigost2020@users.noreply.github.com>
2021-04-26 17:12:08 -0300

    Add files via upload

---
+++ src/DAO/ConexaoDAO.java
@@ -0,0 +1,25 @@
+package DAO;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class ConexaoDAO {
+    
+    public Connection conectaBD(){
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost3306/bancoteste?user=root&password=2536opet";
+            conn = DriverManager.getConnection(url);
+                    
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null,"ConexaoDAO" +  erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
---
+++ src/DTO/UsuarioDTO.java
@@ -0,0 +1,54 @@
+package DTO;
+
+/**
+ *
+ * @author rodri
+ */
+public class UsuarioDTO {
+
+    private int Id_Usuario;
+    private String nome_usuario, senha_usuario;
+
+    /**
+     * @return the Id_Usuario
+     */
+    public int getId_Usuario() {
+        return Id_Usuario;
+    }
+
+    /**
+     * @param Id_Usuario the Id_Usuario to set
+     */
+    public void setId_Usuario(int Id_Usuario) {
+        this.Id_Usuario = Id_Usuario;
+    }
+
+    /**
+     * @return the nome_usuario
+     */
+    public String getNome_usuario() {
+        return nome_usuario;
+    }
+
+    /**
+     * @param nome_usuario the nome_usuario to set
+     */
+    public void setNome_usuario(String nome_usuario) {
+        this.nome_usuario = nome_usuario;
+    }
+
+    /**
+     * @return the senha_usuario
+     */
+    public String getSenha_usuario() {
+        return senha_usuario;
+    }
+
+    /**
+     * @param senha_usuario the senha_usuario to set
+     */
+    public void setSenha_usuario(String senha_usuario) {
+        this.senha_usuario = senha_usuario;
+    }
+
+}
